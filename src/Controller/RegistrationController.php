543529f679287fc832df3a2ebd141e7f7d9bc945
bernard-ng <ngandubernard@gmail.com>
2025-01-21 01:15:03 +0200

    cs fix with rector

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -20,7 +20,7 @@ use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
-    public function __construct(private EmailVerifier $emailVerifier)
+    public function __construct(private readonly EmailVerifier $emailVerifier)
     {
     }
 
